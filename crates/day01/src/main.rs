94233a09ee88859d01ea3307eaf88dd682635059
JacobBorch <jacob.borch@outlook.com>
2025-12-01 18:31:34 +0100

    scaffold day one

---
+++ crates/day01/src/main.rs
@@ -0,0 +1,34 @@
+use anyhow::Result;
+use aoc_lib::read_input;
+
+fn part1(input: &str) -> String {
+    todo!()
+}
+
+fn part2(input: &str) -> String {
+    todo!()
+}
+
+fn main() -> Result<()> {
+    let input = read_input(1)?;
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = include_str!("../../../inputs/01-example.txt");
+
+    #[test]
+    fn part1_example() {
+        assert_eq!(part1(EXAMPLE), "TODO");
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(part2(EXAMPLE), "TODO");
+    }
+}
