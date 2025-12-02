9a6c7b994928572b5ccd40a9979f4bdaf13838f8
JacobBorch <jacob.borch@outlook.com>
2025-12-02 21:10:54 +0100

    cleanup lib function

--- crates/aoc-lib/src/lib.rs
+++ crates/aoc-lib/src/lib.rs
@@ -1,18 +1,7 @@
-use std::{
-    fs::{self},
-    path::PathBuf,
-};
-
-use anyhow::Context;
+use std::path::PathBuf;
 
 pub fn input_path(day: u8) -> PathBuf {
     PathBuf::from(format!("inputs/{:02}.txt", day))
-}
-
-pub fn read_input(day: u8) -> anyhow::Result<String> {
-    let path = input_path(day);
-    fs::read_to_string(&path)
-        .with_context(|| format!("failed to read AoC input at {}", path.display()))
 }
 
 pub fn modulo(a: i32, m: i32) -> i32 {
