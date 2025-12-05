1aee63a954ccedf51894cfd7a69e21446f3152a7
JacobBorch <jacob.borch@outlook.com>
2025-12-05 09:44:48 +0100

    solve part1

--- crates/day05/src/main.rs
+++ crates/day05/src/main.rs
@@ -1,7 +1,36 @@
+use std::{ops::RangeInclusive, usize};
+
 use anyhow::Result;
 
+fn parse_ranges(input: &str) -> Vec<RangeInclusive<usize>> {
+    input
+        .lines()
+        .take_while(|s| !s.trim().is_empty())
+        .map(|s| {
+            let (s1, s2) = s.split_once("-").unwrap();
+            (
+                s1.parse().unwrap(),
+                s2.parse().unwrap(),
+            )
+        })
+        .map(|(a, b)| a..=b)
+        .collect()
+}
+
+fn parse_ids(input: &str) -> Vec<usize> {
+    input.lines().filter_map(|s| s.parse().ok()).collect()
+}
+
+fn solve(input: &str) -> usize {
+    let ranges = parse_ranges(input);
+    let ids = parse_ids(input);
+    ids.into_iter()
+        .filter(|id| ranges.iter().any(|range| range.contains(id)))
+        .count()
+}
+
 fn part1(input: &str) -> String {
-    todo!()
+    solve(input).to_string()
 }
 
 fn part2(input: &str) -> String {
@@ -23,7 +52,7 @@ mod tests {
 
     #[test]
     fn part1_example() {
-        assert_eq!(part1(EXAMPLE), "TODO");
+        assert_eq!(part1(EXAMPLE), "3");
     }
 
     #[test]
