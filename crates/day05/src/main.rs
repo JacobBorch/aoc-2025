65c34538ac3d13138c58bc9a1980c381cda2c29a
JacobBorch <jacob.borch@outlook.com>
2025-12-05 10:40:12 +0100

    impl day5 part2

--- crates/day05/src/main.rs
+++ crates/day05/src/main.rs
@@ -8,10 +8,7 @@ fn parse_ranges(input: &str) -> Vec<RangeInclusive<usize>> {
         .take_while(|s| !s.trim().is_empty())
         .map(|s| {
             let (s1, s2) = s.split_once("-").unwrap();
-            (
-                s1.parse().unwrap(),
-                s2.parse().unwrap(),
-            )
+            (s1.parse().unwrap(), s2.parse().unwrap())
         })
         .map(|(a, b)| a..=b)
         .collect()
@@ -19,6 +16,29 @@ fn parse_ranges(input: &str) -> Vec<RangeInclusive<usize>> {
 
 fn parse_ids(input: &str) -> Vec<usize> {
     input.lines().filter_map(|s| s.parse().ok()).collect()
+}
+
+fn merge_ranges(mut input_ranges: Vec<RangeInclusive<usize>>) -> Vec<RangeInclusive<usize>> {
+    let mut output: Vec<RangeInclusive<usize>> = Vec::new();
+    input_ranges.sort_by(|r1, r2| r1.start().cmp(r2.start()));
+    let mut current_opt = None;
+    for range in &input_ranges {
+        if current_opt.is_none() {
+            current_opt = Some((range.start(), range.end()));
+            continue;
+        }
+        let current = current_opt.unwrap();
+        if range.start() <= current.1 {
+            current_opt = Some((current.0.min(range.start()), current.1.max(range.end())));
+            continue;
+        }
+        output.push(*current.0..=*current.1);
+        current_opt = Some((range.start(), range.end()));
+    }
+    if let Some((&a, &b)) = current_opt {
+        output.push(a..=b);
+    }
+    output
 }
 
 fn solve(input: &str) -> usize {
@@ -34,7 +54,12 @@ fn part1(input: &str) -> String {
 }
 
 fn part2(input: &str) -> String {
-    todo!()
+    let ranges = parse_ranges(input);
+    merge_ranges(ranges)
+        .into_iter()
+        .map(|range| range.end() - range.start() + 1)
+        .sum::<usize>()
+        .to_string()
 }
 
 fn main() -> Result<()> {
@@ -57,6 +82,6 @@ mod tests {
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2(EXAMPLE), "TODO");
+        assert_eq!(part2(EXAMPLE), "14");
     }
 }
