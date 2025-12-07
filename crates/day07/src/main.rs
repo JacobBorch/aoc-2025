658324b88fa2abaad3302239dad9f48aa746ce6c
JacobBorch <jacob.borch@outlook.com>
2025-12-07 21:05:50 +0100

    solve part 1

--- crates/day07/src/main.rs
+++ crates/day07/src/main.rs
@@ -1,7 +1,36 @@
 use anyhow::Result;
 
+fn solve(input: &str) -> usize {
+    let mut splits = 0;
+
+    let grid_length = input.lines().next().unwrap().len();
+    let mut old_beams = vec![false; grid_length];
+
+    for line in input.lines() {
+        let mut new_beams = vec![false; grid_length];
+        for (i, c) in line.chars().enumerate() {
+            match c {
+                '^' => {
+                    if !old_beams[i] {
+                        continue;
+                    }
+                    new_beams[i - 1] = true;
+                    new_beams[i + 1] = true;
+                    splits += 1;
+                }
+                '.' => new_beams[i] = old_beams[i] || new_beams[i],
+                'S' => new_beams[i] = true,
+                _ => unreachable!(),
+            }
+        }
+        old_beams = new_beams;
+    }
+
+    splits
+}
+
 fn part1(input: &str) -> String {
-    todo!()
+    solve(input).to_string()
 }
 
 fn part2(input: &str) -> String {
@@ -23,7 +52,7 @@ mod tests {
 
     #[test]
     fn part1_example() {
-        assert_eq!(part1(EXAMPLE), "TODO");
+        assert_eq!(part1(EXAMPLE), "21");
     }
 
     #[test]
